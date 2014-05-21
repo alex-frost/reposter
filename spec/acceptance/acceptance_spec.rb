afbb9cf30ef0e50a339cddad95759dab43d72b0c
Alex Frost <alex.frost.dev@gmail.com>
2014-05-21 09:15:08 +0100

    start acceptance spec

---
+++ spec/acceptance/acceptance_spec.rb
@@ -0,0 +1,22 @@
+feature "RePoster", type: :feature do
+  let(:heroku_data) { {"key" => "value" } }
+  scenario "add a url and post" do
+    visit "/"
+
+    click_on 'Add url'
+
+    fill_in :url, with: "www.example.com"
+
+    post "/", heroku_data
+
+    check_data_is_posted
+  end
+
+  def check_data_is_posted
+    #not sure how to test this yet
+  end
+
+end
+
+
+
