96d43234ea06d8ddda0a79004abc566e9bfa485b
Alex Frost <alex.frost.dev@gmail.com>
2014-05-21 11:26:38 +0100

    accept any POST params

--- app/controllers/repost_controller.rb
+++ app/controllers/repost_controller.rb
@@ -5,12 +5,8 @@ class RepostController < ApplicationController
     logger.info "posting"
     logger.info params
     RepostUrl.all.each do |r|
-      PostToUrl.perform({url: r.url, params: repost_params})
+      PostToUrl.perform({url: r.url, params: params})
     end
   end
 
-  private
-    def repost_params
-      params.require(:repost)
-    end
 end
